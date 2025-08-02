eff5c0e46e47d715250584d7091b642182afeb48
BaoHuynh888 <baohuynh888@gmail.com>
2025-08-01 17:12:19 -0700

    tsx to js

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <script type="module" src="./src/main.tsx"></script>
+    <script type="module" src="/main.js"></script>
     <App />
   </StrictMode>,
 )
